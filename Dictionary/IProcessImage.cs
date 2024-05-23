b41f0083fc8d692132cc69829eabbec1d3b8791c
AngeHdz <a01794256@tec.mx>
2024-05-23 16:16:23 -0500

    Crear ProcessStep

---
+++ Dictionary/IProcessImage.cs
@@ -0,0 +1,15 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguClass.Dictionary
+{
+    public interface IProcessImage
+    {
+        Image<Bgr, byte> Process(Image<Bgr, byte> image);
+    }
+}
---
+++ Process/ProcessStep.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmguClass.Process
+{
+    public class ProcessStep
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public ProcessStep(string name, string type)
+        {
+            Name = name;
+            this.Type = type;
+        }
+    }
+}
